6e489696ab1268b8981e6f687988dfeedc00dd17
clane9 <connor.lane858@gmail.com>
2018-02-27 15:13:09 -0500

    Add documentation and some reorg to SDLSC_MC.

--- solvers/SDLSC_MC.m
+++ solvers/SDLSC_MC.m
@@ -5,23 +5,56 @@ classdef SDLSC_MC < SC_MC_Base_Solver
 %   min_{D,C} lambda/2 ||P_Omega(X - UC)||_F^2 ...
 %       + 1/2 ||U||_F^2 + eta_1 ||C||_1 + eta_2/2 ||C||_F^2
 %
-%   solver = SDLSC_MC(X, Omega, n, lambda, eta1, eta2)
+%   solver = SDLSC_MC(X, Omega, n, lambda, eta1, eta2, K)
 
   properties
-    eta1; eta2;
+    eta1; eta2; K;
   end
 
   methods
 
-    function self = SDLSC_MC(X, Omega, n, lambda, eta1, eta2)
+    function self = SDLSC_MC(X, Omega, n, lambda, eta1, eta2, K)
+    % SDLSC_MC    Solver for SDL based combined subspace clustering and completion.
+    %   Solves formulation
+    %
+    %   min_{D,C} lambda/2 ||P_Omega(X - UC)||_F^2 ...
+    %       + 1/2 ||U||_F^2 + eta_1 ||C||_1 + eta_2/2 ||C||_F^2
+    %
+    %   solver = SDLSC_MC(X, Omega, n, lambda, eta1, eta2, K)
+    %
+    %   Args:
+    %     X: D x N incomplete data matrix.
+    %     Omega: D x N binary pattern of missing entries.
+    %     n: number of clusters.
+    %     lambda: self-expression penalty parameter.
+    %     eta1: sparsity penalty parameter for C.
+    %     eta2: L2 penalty parameter for C.
+    %     K: size of dictionary.
+    %
+    %   Returns:
+    %     self: SDLSC_MC solver instance.
     if nargin < 6; eta2 = 1; end
+    if nargin < 7; K = ceil(0.2*size(X,2)); end
     self = self@SC_MC_Base_Solver(X, Omega, n, lambda);
     self.eta1 = eta1;
     self.eta2 = eta2;
+    self.K = K;
     end
 
 
     function [obj, L, R] = objective(self, U, C)
+    % objective   Evaluate objective, loss, and regularization.
+    %
+    %   [obj, L, R] = solver.objective(U, C)
+    %
+    %   Args:
+    %     U: D x K dictionary.
+    %     C: K x N sparse representation coefficient C.
+    %
+    %   Returns:
+    %     obj: Objective value.
+    %     L: Reconstruction loss.
+    %     R: regularizer.
     Res = self.X - U*C;
     L = 0.5*sum(Res(self.Omega).^2);
     R = 0.5*sum(U(:).^2) + self.eta1*sum(abs(C(:))) + self.eta2*0.5*sum(C(:).^2);
@@ -38,10 +71,8 @@ classdef SDLSC_MC < SC_MC_Base_Solver
     %       solveU_params)
     %
     %   Args:
-    %     U0: Initial guess for dictionary U. If empty, will initialize with
-    %        N elements of ZF data.
+    %     U0: Initial guess for dictionary U. If empty, will initialize with ZF data.
     %     params: struct containing parameters for optimization:
-    %       K: Size of default dictionary [default: 0.2*N].
     %       maxIter: maximum iterations [default: 30].
     %       maxTime: maximum time allowed in seconds [default: Inf].
     %       convThr: convergence threshold [default: 1e-6].
@@ -70,9 +101,9 @@ classdef SDLSC_MC < SC_MC_Base_Solver
     if nargin < 2; params = struct; end
     if nargin < 3; exprC_params = struct; end
     if nargin < 4; solveU_params = struct; end
-    fields = {'K', 'maxIter', 'maxTime', 'convThr', 'lambdaIncr', ...
+    fields = {'maxIter', 'maxTime', 'convThr', 'lambdaIncr', ...
         'lambdaIncrSteps', 'trueData', 'prtLevel', 'logLevel'};
-    defaults = {ceil(0.2*self.N), 30, Inf, 1e-6, 1, 1, {}, 1, 1};
+    defaults = {30, Inf, 1e-6, 1, 1, {}, 1, 1};
     for ii=1:length(fields)
       if ~isfield(params, fields{ii})
         params.(fields{ii}) = defaults{ii};
@@ -99,19 +130,19 @@ classdef SDLSC_MC < SC_MC_Base_Solver
     prtformstr = [prtformstr ' \n'];
 
     if isempty(U0)
-      subinds = randperm(self.N, params.K);
+      subinds = randperm(self.N, self.K);
       U = self.X(:,subinds);
     else
-      U = U0;
+      U = U0; self.K = size(U0,2);
     end
-    K = size(U,2); C = zeros(K, self.N);
+    C = zeros(self.K, self.N);
     relthr = infnorm(self.X(self.Omega));
     U_last = U; C_last = C; obj_last = self.objective(U, C);
     history.status = 1;
     for kk=1:params.maxIter
       if kk > 1
         % Prune dictionary as in K-SVD, to try to avoid local-minima.
-        U = self.pruneU(U,C);
+        % U = self.pruneU(U,C);
       end
       % Alternate updating C, U.
       [C, exprC_history] = self.exprC(U, C, exprC_params);
@@ -178,9 +209,24 @@ classdef SDLSC_MC < SC_MC_Base_Solver
 
 
     function [C, history] = exprC(self, U, ~, ~)
+    % exprC   Compute self-expression with elastic-net regularization using
+    %   SPAMS mexLasso.  Solves the formulation
+    %
+    %   min_C \lambda/2 ||W \odot (X - UC)||_F^2 + ...
+    %     \eta_1 ||C||_1 + eta_2/2 ||C||_F^2
+    %
+    %   [C, history] = solver.exprC(U, ~, ~)
+    %
+    %   Args:
+    %     U: D x K dictionary.
+    %     C0: K x N self-expressive coefficient initial guess (not used).
+    %     params: (not used, included for consistency.)
+    %
+    %   Returns:
+    %     C: K x N self-expression.
+    %     history: Struct containing diagnostic info.
     tstart = tic;
-    K = size(U,2);
-    C = zeros(K, self.N);
+    C = zeros(self.K, self.N);
     % Convert to spams notation.
     spams_param.lambda = self.eta1/self.lambda;
     spams_param.lambda2 = self.eta2/self.lambda;
@@ -197,11 +243,25 @@ classdef SDLSC_MC < SC_MC_Base_Solver
 
 
     function [U, history] = solveU(self, ~, C, ~)
+    % solveU   Update dictionary by solving least-squares problem. Solves the
+    %   formulation
+    %
+    %   min_U \lambda/2 ||W \odot (X - UC)||_F^2 + 1/2 ||U||_F^2
+    %
+    %   [U, history] = solver.solveU(~, C, ~)
+    %
+    %   Args:
+    %     U0: D x K dictionary initial guess (not used).
+    %     C: K x N self-expressive coefficient.
+    %     params: (not used, included for consistency.)
+    %
+    %   Returns:
+    %     U: D x K self-expression.
+    %     history: Struct containing diagnostic info.
     tstart = tic;
-    K = size(C,1);
-    U = zeros(self.D, K);
+    U = zeros(self.D, self.K);
     CT = C';
-    eyeK = eye(K);
+    eyeK = eye(self.K);
     for ii=1:self.D
       % solve: lambda/2 || (x_i)_{omega_i} - u_i C diag(omega_i)||_2^2 + ...
       % by dropping unobserved columns of x_i, C (note x_i, omega_i, u_i row vectors.)
@@ -215,10 +275,22 @@ classdef SDLSC_MC < SC_MC_Base_Solver
 
 
     function U = pruneU(self, U, C, toosmall_thr, toosim_thr)
+    % pruneU    prune dictionary following k-SVD.
+    %
+    %   U = solver.pruneU(U, C, toosmall_thr, toosim_thr)
+    %
+    %   Args:
+    %     U: D x K dictionary.
+    %     C: K x N sparse representation.
+    %     toosmall_thr: threshold for atom norms to be too small [default: 1e-4].
+    %     toosim_thr: threshold for atoms to be too similar (cos angle)
+    %       [default: 0.9].
+    %
+    %   Returns:
+    %     U: pruned dictionary.
     if nargin < 4; toosmall_thr = 1e-4; end
-    if nargin < 4; toosim_thr = 0.9; end
+    if nargin < 5; toosim_thr = 0.9; end
     % Get indices of dictionary elements not used enough.
-    K = size(U,2);
     Unorms = sum(U.^2); Cnorms = sum(C.^2,2)';
     combnorms = 0.5*(Unorms + Cnorms);
     toosmallmask = combnorms < toosmall_thr*max(combnorms);
@@ -226,7 +298,9 @@ classdef SDLSC_MC < SC_MC_Base_Solver
     normU = U./repmat(sqrt(Unorms)+eps, [self.D 1]);
     G = normU'*normU;
     % Set lower triangle (including diagonal) to 0.
-    I = repmat((1:K)', [1 K]); J = repmat(1:K, [K 1]); triumask = I<=J;
+    I = repmat((1:self.K)', [1 self.K]);
+    J = repmat(1:self.K, [self.K 1]);
+    triumask = I<=J;
     G(triumask) = 0;
     dotprods = max(abs(G));
     toosimmask = dotprods > toosim_thr;
@@ -250,13 +324,12 @@ classdef SDLSC_MC < SC_MC_Base_Solver
 
     % Normalized-cut spectral clustering for bipartite affinity graph.
     % See: Adler et al., "Linear-time subspace clustering", 2015.
-    [K, N] = size(C);
     absC = abs(C);
     % Threshold small values to zero.
     absC(absC < 1e-8) = 0; absC = sparse(absC);
 
-    D1 = spdiags(1./sqrt(sum(absC,2)+eps), 0, K, K);
-    D2 = spdiags(1./sqrt(sum(absC)+eps)', 0, N, N);
+    D1 = spdiags(1./sqrt(sum(absC,2)+eps), 0, self.K, self.K);
+    D2 = spdiags(1./sqrt(sum(absC)+eps)', 0, self.N, self.N);
     [U, ~, V] = svds(D1*absC*D2, self.n);
     Z = [D1*U; D2*V];
 
@@ -264,10 +337,10 @@ classdef SDLSC_MC < SC_MC_Base_Solver
     REPlic = 20; % Number of replications for KMeans
     groups = kmeans(Z, self.n, 'maxiter', MAXiter, 'replicates', REPlic);
     % First K elements correspond to dictionary atoms.
-    groups = groups((K+1):end);
+    groups = groups((self.K+1):end);
 
     % Affinity matrix computed for visualization and post-processing only.
-    A = [sparse(K+N, K) [absC; sparse(N, N)]];
+    A = [sparse(self.K+self.N, self.K) [absC; sparse(self.N, self.N)]];
     A = A + A';
     if nargin > 2
       [cluster_err, groups] = eval_cluster_error(groups, groupsTrue);
