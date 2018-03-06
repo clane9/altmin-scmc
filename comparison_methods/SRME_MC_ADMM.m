4139581a9292f61ab4ab44030077fda0493c01bd
clane9 <connor.lane858@gmail.com>
2018-03-05 21:27:51 -0500

    Minor changes to SRME_MC_ADMM
    - Add paramers maxIter, maxTime, convThr to control runtime.
    - Add initialization argument.

--- comparison_methods/SRME_MC_ADMM.m
+++ comparison_methods/SRME_MC_ADMM.m
@@ -1,7 +1,10 @@
-function [X,Z,E]=SRME_MC_ADMM(X0,M,lambda,alpha)
+function [X,Z,E]=SRME_MC_ADMM(X0,M,Xinit,lambda,alpha,params)
 % X0---matrix with missing entries
 % M---indices binary (0-1) matrix, 1/0 indicates observed/missing entries
+% Xinit---Initialization for completion X. If empty, will take Xinit=X0.
 % lambda and alpha are regularization parameters
+% params---struct containing maxIter [default:800], maxTime [default:Inf],
+%   convThr [default:1e-6].
 
 % X---recovered matrix
 % Z---self-representation coefficients matrix
@@ -12,10 +15,26 @@ function [X,Z,E]=SRME_MC_ADMM(X0,M,lambda,alpha)
 % J Fan, TWS Chow. Neural Networks 2017(93):36-44
 
 % Written by Jicong Fan.
+% Edited by Connor Lane:
+%   - Added some parameters to control runtime.
+%   - Added initialization for X (completion).
+
+if nargin < 6; params = struct; end
+fields = {'maxIter', 'maxTime', 'convThr'};
+defaults = {800, Inf, 1e-6};
+for ii=1:length(fields)
+    if ~isfield(params, fields{ii})
+        params.(fields{ii}) = defaults{ii};
+    end
+end
+tstart = tic;
 
 [d,n]=size(X0);
 %
-X=X0;
+if isempty(Xinit)
+    Xinit=X0;
+end
+X=Xinit;
 Y=X0;
 E=zeros(d,n);
 Z=zeros(n,n);
@@ -24,11 +43,9 @@ Q1=zeros(d,n);
 Q2=zeros(n,n);
 Q3=zeros(d,n);
 %
-maxIter=800;
 u=0.1;
 u_max=1e10;
 r0=1.01;
-e=1e-6;
 %
 normF_X0=norm(X0,'fro');
 %
@@ -40,7 +57,7 @@ for i=1:n
 end
 g=ones(1,d)/d;
 %
-while iter<maxIter
+while iter<params.maxIter
     iter=iter+1;
     % A_new
     temp=X'*(X-E+Q1/u)+Z-Q2/u;
@@ -68,10 +85,10 @@ while iter<maxIter
     stopC2=max([norm(Y_new-X_new,'fro') norm(A_new-Z_new,'fro')])/normF_X0;
     stopC3=max([norm(A_new-A,'fro') norm(Z_new-Z,'fro') norm(X_new-X,'fro') norm(E_new-E,'fro')])/normF_X0;
     %
-    isstopC=max([stopC1 stopC2 stopC3])<e;
+    isstopC=max([stopC1 stopC2 stopC3])<params.convThr;
     if mod(iter,100)==0||isstopC
         disp(['rankX=' num2str(rank(X_new,1e-3*norm(X_new,2)))])% can be closed for acceleration
-        disp(['iteration=' num2str(iter) '/' num2str(maxIter)])
+        disp(['iteration=' num2str(iter) '/' num2str(params.maxIter)])
         disp(['stopC1=' num2str(stopC1)])
         disp(['stopC2=' num2str(stopC2)])
         disp(['stopC3=' num2str(stopC3)])
@@ -82,6 +99,10 @@ while iter<maxIter
         disp('converged')
         break;
     end
+    if toc(tstart) >= params.maxTime
+        disp('timeout!')
+        break;
+    end
     %
     r=r0;
     u=min(u_max,r*u);
@@ -90,9 +111,9 @@ while iter<maxIter
     X=X_new;
     Y=Y_new;
     E=E_new;
-        %     
+        %
 end
-      
+
 end
 
 
@@ -109,5 +130,3 @@ function [Z]=solve_NuclearNorm(L,mu)
     end
     Z=U(:,1:svp)*diag(sigma)*V(:,1:svp)';
 end
-
-
